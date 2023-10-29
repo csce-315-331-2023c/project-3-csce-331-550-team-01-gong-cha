48146f8fdb091f3ab50eb647f5983c012c0cb7c2
AsunaIsMyDaddy <93293753+AsunaIsMyDaddy@users.noreply.github.com>
2023-10-29 17:54:44 -0500

    created home page

---
+++ client/src/app/Order/page.tsx
@@ -0,0 +1,7 @@
+export default function Order() {
+    return (
+      <main >
+        <h1>Soon to Be Order Page</h1>
+      </main>
+    );
+  }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,150 +1,38 @@
 'use client'
 import Image from 'next/image'
+import TEA from '../../public/homeBackground.png'
 
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 
-export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+export default function Dashboard() {
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  function managerLogin(){
+    alert("Add Manager Login");
+  }
 
-    // Simulated user data (in a real app, this would come from a server)
-    const users = [
-      { email: 'user1@example.com', password: 'password1' },
-      { email: 'user2@example.com', password: 'password2' },
-    ];
-
-    const user = users.find((user) => user.email === email && user.password === password);
-
-    if (user) {
-      // Redirect to the dashboard or perform other actions here
-      window.location.href = '/dashboard';
-    } else {
-      // Display an error message
-      setError('Invalid email or password');
-    }
-  };
+  function gotToOrder(){
+    window.location.href = "/Order";
+  }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <motion.div
-        className="bg-gradient-to-r from-indigo-600 to-pink-600 py-[85px] text-center relative"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <motion.h1
-          className="text-5xl font-extrabold text-white mb-4"
-          initial={{ y: -20 }}
-          animate={{ y: 0 }}
-          transition={{ duration: 0.6, delay: 0.2 }}
-        >
-          Welcome to Our App
-        </motion.h1>
-        <motion.p
-          className="text-xl text-white mb-6"
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
-        >
-          Please log in to access your account.
-        </motion.p>
-      </motion.div>
-      <motion.div
-        className="flex-1 bg-gray-200 flex flex-col justify-center items-center"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <motion.div
-          className="bg-white p-[90px] rounded-lg shadow-lg"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.6, delay: 0.2 }}
-        >
-          <motion.h1
-            className="text-2xl font-semibold text-center text-gray-800 mb-4"
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.4 }}
-          >
-            Login
-          </motion.h1>
-          <form onSubmit={handleSubmit}>
-            <motion.div
-              className="mb-4 flex items-center"
-              initial={{ opacity: 0, x: -20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.4 }}
-            >
-              <label htmlFor="email" className="block text-sm text-gray-600">
-                Email:
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="example@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className="w-full border-b border-gray-300 p-3 focus:outline-none focus:ring focus:ring-indigo-400"
-              />
-            </motion.div>
-            <motion.div
-              className="mb-4 flex items-center"
-              initial={{ opacity: 0, x: -20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.4 }}
-            >
-              <label htmlFor="password" className="block text-sm text-gray-600">
-                Password:
-              </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Your Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                className="w-full border-b border-gray-300 p-3 focus:outline-none focus:ring focus:ring-indigo-400"
-              />
-            </motion.div>
-            <motion.button
-              type="submit"
-              className="w-full bg-indigo-500 text-white py-3 rounded-[20px] hover:bg-indigo-600"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.4 }}
-            >
-              Login
-            </motion.button>
-          </form>
-          <motion.p
-            className="text-red-500 mt-4"
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.4 }}
-          >
-            {error}
-          </motion.p>
-        </motion.div>
-      </motion.div>
-      <motion.div
-        className="bg-gray-100 text-center p-4"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <p className="text-gray-600">
-          Your Company &copy; {new Date().getFullYear()}
-        </p>
-      </motion.div>
+    <div className='h-screen'>
+        <Image
+        className='back -z-10'
+        src={TEA}
+        alt='Background image of tea leaves being processed'
+        layout='fill'
+        objectFit='cover'
+        />
+    <div className='flex justify-end items-right w-full h-1/6'>
+      <button onClick={managerLogin}>
+        <svg className='pt-8 pr-12 fill-rose-700' xmlns="http://www.w3.org/2000/svg" height="8em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+      </button> 
     </div>
+    <div className='flex justify-center items-center w-full h-5/6'>
+      <button onClick={gotToOrder} className='bg-rose-700 h-1/6 w-1/6'>
+        Start Order
+      </button>
+    </div>
+  </div>
   );
 }
-
-
