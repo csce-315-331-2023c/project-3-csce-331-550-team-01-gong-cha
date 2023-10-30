784c6f7bfb701d98153ab728f67251b1fc374ac6
AsunaIsMyDaddy <93293753+AsunaIsMyDaddy@users.noreply.github.com>
2023-10-30 15:22:31 -0500

    added drink subsections

--- client/src/app/Components/MenuItem/MenuItem.tsx
+++ client/src/app/Components/MenuItem/MenuItem.tsx
@@ -6,14 +6,14 @@ import Image from 'next/image'
 export default function MenuItem({drinkName, drinkImage, altTxt, thisOnClick}){
     
     return(
-        <button className='w-full h-full flex justify-start items-start bg-indigo-300' onClick={thisOnClick}>
+        <button className='w-full h-full flex justify-center items-center bg-indigo-300' onClick={thisOnClick}>
             <Image
                 className='h-full'
                 src={drinkImage}
                 alt={altTxt}
-            ></Image>
-            <div className='bg-green-300 w-full'>
-                <div className='flex justify-center items-center w-full'>
+            />
+            <div className='w-full'>
+                <div className='flex justify-center items-center w-full text-5xl'>
                     {drinkName}
                 </div>
             </div>
--- client/src/app/Order/Catagories/MilkFoam/page.tsx
+++ client/src/app/Order/Catagories/MilkFoam/page.tsx
@@ -1,48 +1,58 @@
 "use client"
 
-import Image from 'next/image'
-import React from 'react'
 import MenuItem from '../../../Components/MenuItem/MenuItem'
-import defualtDrinkImg from '../../../public/defualtDrinkImg.png'
+import defualtDrinkImg from '../../../../../public/defualtDrinkImg.png'
+import '../../styles.css'
 
-export default function MilkFoam() {
-  function something(){
-    alert("Something happens")
-  }
-  return (
-    <main className="bg-slate-400 bg-cover h-screen w-screen w-screenflex-row flex">
-      <div className="flex flex-col items-center justify-start w-2/5 h-screen m-4">
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-      </div>
-      <div className="flex flex-col items-center justify-start w-2/5 h-screen  m-4">
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-          <div className='h-1/5 w-full m-4'>
-              <MenuItem drinkName={"Some Drink"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={() => something()}/>
-          </div>
-      </div>
+export default function Order() {
 
-      <div className="w-1/5 ml-14 my-10">
-          <div className="bg-white h-full w-4/5 border-gray border-4 rounded-2xl text-center"> Orders </div>
-      </div>
-    </main>
-  );
+    function goToCustomization(){
+        alert("Customization");
+    }
+    function goBack(){
+        window.location.href = "../";
+    }
+    return (
+        <main className="bg-slate-400 bg-cover h-screen w-screen w-screenflex-row flex flex-col">
+            <button className='backContainter flex items-center' onClick={goBack}>
+                <svg className='ml-4' xmlns="http://www.w3.org/2000/svg" height="5em" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                <div className='ml-8 text-4xl'>Catagories</div>
+                <div className='ml-80 text-6xl'>Milk Foam</div>
+            </button>
+            <div className='catagoryContainer w-screen w-screenflex-row flex h-full'>
+                <div className="flex flex-col items-center justify-start w-2/5 h-full m-4">
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Dirty Brown Sugar Milk Tea"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Milk Foam Earl Grey Tea"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Milk Foam Oolong Tea"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Creme Brulee Brown Sugar Milk Tea"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-start w-2/5 h-full m-4">
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Milk Foam Black Tea"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Milk Foam Green Tea"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Milk Foam Wintermelon"} drinkImage={defualtDrinkImg} altTxt={"Test Drinkx`"} thisOnClick={goToCustomization}/>
+                    </div>
+                    <div className='h-1/5 w-full m-4'>
+                        <MenuItem drinkName={"Creme Brulee Strawberry Latte"} drinkImage={defualtDrinkImg} altTxt={"Test Drink"} thisOnClick={goToCustomization}/>
+                    </div>
+                </div>
+
+                <div className="w-1/5 ml-14 my-10">
+                    <div className="bg-white h-full w-4/5 border-gray border-4 rounded-2xl text-center"> Orders </div>
+                </div>
+            </div>
+        </main>
+    );
 }
