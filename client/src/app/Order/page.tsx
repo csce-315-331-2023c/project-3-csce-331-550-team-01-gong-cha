bee4aa5dabbbe705c7a9c334d8bd38d2e2d087ba
Dyllenf <dyllenfau@gmail.com>
2023-10-30 10:29:03 -0500

    ready to test components

--- client/src/app/Order/page.tsx
+++ client/src/app/Order/page.tsx
@@ -1,7 +1,20 @@
 export default function Order() {
     return (
-      <main >
-        <h1>Soon to Be Order Page</h1>
+      <main className="bg-slate-400 bg-cover h-screen flex-row flex ">
+        <div className="grid grid-cols-2 gap-4 h-screen bg-white w-4/5">
+          <div className="grid grid-cols-2 gap-4 border-black border-2">01</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">02</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">03</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">04</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">05</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">06</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">07</div>
+          <div className="grid grid-cols-2 gap-4 border-black border-2">08</div>
+      </div>
+
+      <div className="w-1/5 mx-14 my-10">
+        <div className="bg-white h-5/6 w-4/5 border-gray border-4 rounded-2xl"> Orders </div>
+      </div>
       </main>
     );
   }
---
+++ client/src/app/examplePage/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <main className="flex w-screen h-screen p-[15px] pt-[50px]">
+      <h1>Meant to Show SubPage Structure</h1>
+    </main>
+  )
+}
