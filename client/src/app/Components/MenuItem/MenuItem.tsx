f28f9e699f93b272799be5dd0637f68e71d66b5f
AsunaIsMyDaddy <93293753+AsunaIsMyDaddy@users.noreply.github.com>
2023-10-30 11:16:45 -0500

    working on menu items with components

---
+++ client/src/app/Components/MenuItem/MenuItem.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Image from 'next/image'
+
+export default function MenuItem({drinkName, drinkImage, altTxt}){
+
+    return(
+        <main className='w-full h-full flex justify-start items-start bg-indigo-300'>
+            <Image
+                className='h-full'
+                src={drinkImage}
+                alt={altTxt}
+            ></Image>
+            <div className='bg-green-300 w-full'>
+                <div className='flex justify-center items-center w-full'>
+                    {drinkName}
+                </div>
+            </div>
+        </main>
+    );
+}
--- client/src/app/Order/page.tsx
+++ client/src/app/Order/page.tsx
@@ -1,20 +1,39 @@
+import MenuItem from '../Components/MenuItem/MenuItem'
+import defualtDrinkImg from '../../../public/defualtDrinkImg.png'
+
 export default function Order() {
     return (
-      <main className="bg-slate-400 bg-cover h-screen flex-row flex ">
-        <div className="grid grid-cols-2 gap-4 h-screen bg-white w-4/5">
-          <div className="grid grid-cols-2 gap-4 border-black border-2">01</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">02</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">03</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">04</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">05</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">06</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">07</div>
-          <div className="grid grid-cols-2 gap-4 border-black border-2">08</div>
-      </div>
+      <main className="bg-slate-400 bg-cover h-screen w-screen w-screenflex-row flex">
+        <div className="grid grid-cols-2 gap-4 h-screen w-4/5 bg-white">
+          <div className="border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className="border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className=" border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className=" border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className=" border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className=" border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className=" border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+          <div className="border-black border-2">
+            <MenuItem drinkName={"Test"} drinkImage={defualtDrinkImg} altTxt={"Defualt Drink Icon"}/>
+          </div>
+        </div>
 
-      <div className="w-1/5 mx-14 my-10">
-        <div className="bg-white h-5/6 w-4/5 border-gray border-4 rounded-2xl"> Orders </div>
-      </div>
+        <div className="w-1/5 mx-14 my-10">
+            <div className="bg-white h-5/6 w-4/5 border-gray border-4 rounded-2xl"> Orders </div>
+        </div>
       </main>
     );
   }
