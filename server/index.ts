57fbc56345d0315ff4e82629774be4333fe4b4c2
rgarrison14515 <rgarrison14515@gmail.com>
2023-11-01 13:47:52 -0500

    Added RestockReport

--- server/index.ts
+++ server/index.ts
@@ -757,6 +757,39 @@ app.get('/get-ideal-ingredients-amount', async (req, res) => {
     res.status(500).json({ error: 'An error occurred while fetching ideal ingredient amounts' });
   }
 });
+//restock Ingredients
+app.get('/report-restock', async (req, res) => {
+  var returnObject = []
+  try {
+    const querySQL = `
+      SELECT ingredient_name, current_amount, ideal_amount
+      FROM ingredient
+      WHERE current_amount < (ideal_amount * 0.4)
+    `;
+
+    const client = await pool.connect();
+    const { rows } = await client.query(querySQL);
+    client.release();
+
+    let result = "| Name of Ingredient                        | Current Amount  | Ideal Amount       |\n";
+
+    for (const row of rows) {
+      const name: string = row.ingredient_name;
+      const currentAmount: number = row.current_amount;
+      const idealAmount: number = row.ideal_amount;
+
+      //result += `| ${name.padEnd(40)} | ${currentAmount.toString().padEnd(15)} | ${idealAmount.toString().padEnd(20)} |\n`;
+      returnObject.push({ingredient_name: name, current_amount: currentAmount, ideal_amount: idealAmount})
+    }
+
+    //res.status(200).send(result);
+    res.status(200).send(returnObject);
+  } catch (error) {
+    console.error('Error getting ingredients to restock:', error);
+    res.status(500).json({ error: 'Error getting ingredients to restock' });
+  }
+});
+
 //the listening happens here
 app.listen(port, () => {
 console.log(`Example listening at  http://localhost:${port}`);
