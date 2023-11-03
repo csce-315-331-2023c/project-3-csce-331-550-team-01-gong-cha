7aba4adf6ab43c2785d2cad717124345ffb90e6c
root <root@ThomasPC>
2023-11-02 20:43:03 -0500

    trying to fix CORS error

--- server/index.ts
+++ server/index.ts
@@ -9,10 +9,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port: number = 5000;
 
-const cors = require('cors');
-app.use(cors({
-    origin: 'http://18.191.166.59:3000/'
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+if (req.method == "OPTIONS") {
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  return res.status(200).json({});
+}
+
+next();
+});
 
 //create pool
 const pool = new Pool({
